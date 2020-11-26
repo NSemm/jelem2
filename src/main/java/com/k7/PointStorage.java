43e55fdea699a3aaae047c2754e8c6f401d47382
NickSemerenko <nickolay.sem@gmail.com>
2020-11-26 22:40:04 +0200

    new methods in PointStorage

--- src/main/java/com/k7/PointStorage.java
+++ src/main/java/com/k7/PointStorage.java
@@ -1,6 +1,5 @@
 package com.k7;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class PointStorage {
@@ -10,8 +9,34 @@ public class PointStorage {
         return points;
     }
 
+    public Point getPoint(int i) {
+        checkIndex(i);
+        return points[i];
+    }
+
+    public void setPoint(int i, Point p) {
+        checkIndex(i);
+        this.points[i] = p;
+    }
+
     public void addPoint(Point point) {
         points = Arrays.copyOf(points, points.length + 1);
         points[points.length - 1] = point;
+    }
+
+    public void removePoint(int i) {
+        checkIndex(i);
+        Point[] temp = new Point[points.length - 1];
+        for (int j = 0; j < temp.length; j++) {
+            temp[j] = points[j < i ? j : j + 1];
+        }
+        points = temp;
+    }
+    public void checkIndex (int i){
+        if (i>= points.length||i<0)
+            throw new IndexOutOfBoundsException();
+    }
+    public int size (){
+        return points.length;
     }
 }
